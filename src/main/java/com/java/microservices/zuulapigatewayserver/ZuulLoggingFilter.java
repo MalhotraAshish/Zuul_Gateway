48819cdc576196dad9d59ad319b91420beb894c1
ltijavab209 <ltijavab209@lti164.iiht.tech>
2020-10-04 00:47:29 +0530

    Zuul Gateway

---
+++ src/main/java/com/java/microservices/zuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,54 @@
+package com.java.microservices.zuulapigatewayserver;
+
+
+
+	
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;  
+	@ComponentScan  
+	public class ZuulLoggingFilter extends ZuulFilter  
+	{  
+	//creating Logger object  
+	private Logger logger=LoggerFactory.getLogger(this.getClass());  
+	public boolean booleanshouldFilter()   
+	{  
+	return true; //executing filter for every request  
+	}  
+	//log the content of the request  
+	@Override  
+	public Object run() throws ZuulException  
+	{  
+	//getting the current HTTP request that is to be handle  
+	HttpServletRequest request=RequestContext.getCurrentContext().getRequest();  
+	//prints the detail of the requestin the log  
+	logger.info("request -> {} request uri-> {}", request, request.getRequestURI());  
+	return null;  
+	}  
+	@Override  
+	public String filterType()   
+	{  
+	return "pre"; //intercept all the request before execution  
+	}  
+	public int intfilterOrder()   
+	{  
+	return 1; //setting filter order to 1  
+	}
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}  
+	}  
